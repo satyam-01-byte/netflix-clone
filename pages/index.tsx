78c029d50809ed75b20e3210238450a030307f7f
satyam-01-byte <satyamsundaram01@gmail.com>
2022-10-02 14:05:48 +0530

    updated favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home = ({netflixOriginals,
     <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.jpeg" />
       </Head>
 
       <Header />
