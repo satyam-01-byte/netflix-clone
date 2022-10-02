ebb85e8fd74c9509429c1a45adb320cf47d36e9b
satyam-01-byte <satyamsundaram01@gmail.com>
2022-10-02 13:40:41 +0530

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,51 @@
+import {MagnifyingGlassIcon, BellIcon} from "@heroicons/react/24/solid"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
+function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        // on unmount: cleanup function
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, []) // [] means only on mount
+
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+        <div className="flex items-center space-x-2 md:space-x-10">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" width={100} height={100} className="cursor-pointer object-contain" />
+
+            <ul className="hidden space-x-4 md:flex">
+                <li className="headerLink">Home</li>
+                <li className="headerLink">TV Shows</li>
+                <li className="headerLink">Movies</li>
+                <li className="headerLink">New & Popular</li>
+                <li className="headerLink">My list</li>
+            </ul>
+        </div>
+
+        <div className="flex items-center space-x-4 text-sm font-light cursor-pointer">
+            <MagnifyingGlassIcon className="h-6 w-6 hidden sm:inline"/>
+            <p className="hidden lg:inline">Kids</p>
+            <BellIcon className="h-6 w-6"/>
+            <Link href="/account">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" className="cursor-pointer rounded" width={30} height={30} />
+            </Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header
